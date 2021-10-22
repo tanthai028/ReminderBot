e0eb084e9d26bcb2e8878ec54f6be46bf4ac1e1b
Tan <35444054+tanthai028@users.noreply.github.com>
2021-10-22 16:34:43 +0000

    created bot command and added more bot command for db

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 # Libraries
 import os
-import asyncio
-import discord
+# import asyncio
 from discord.ext import commands
+import discord
 from datetime import datetime
 import pytz
 import requests
 from replit import db
-# import requests #make http request api
+
 
 
 bot = commands.Bot(command_prefix="!")
 
-# client = discord.Client()
 my_secret = os.environ['Token']  #secret from a file
 
 @bot.event
 async def on_ready():
-    print(f'Logged on as {bot.user.name}!')
+  print(f'Logged on as {bot.user.name}!')
+
+# @bot.event
+# async def on_message(message):
+#    print(f'Message from {message.author}: {message.content}')
+
 
 @bot.command()
 async def date(ctx):
@@ -31,11 +35,11 @@ async def date(ctx):
 
 @bot.command()
 async def meme(ctx):
-
-  url = "https://mem-api.herokuapp.com/gimme"
-  json_data = requests.get(url).json()
-  link = json_data["postLink"]
-  imgUrl = json_data["url"]
+  url = "https://meme-api.herokuapp.com/gimme"
+  response = requests.get(url)
+  json = response.json()
+  link = json["postLink"]
+  imgUrl = json["url"]
   
   await ctx.channel.send(imgUrl)
   await ctx.channel.send(link)
@@ -43,42 +47,45 @@ async def meme(ctx):
 
 @bot.command()
 async def sup(ctx):
-  
   await ctx.channel.send(f"Sup {ctx.author.mention}")
 
 
 @bot.command()
-async def picS(ctx, picName, link):
+async def createDB(ctx, picName, link):
   #save and image
-  if ctx.content.startswith('!picS'):
-    #!picS [picname] [piclink]
-    #check if in db already
-    try:
-      db[picName]
-      await ctx.channel.send("Image already saved!")
+  try:
+    db[picName]
+    await ctx.channel.send("Image already saved!")
 
-    except:
-      db[picName] = link
-      await ctx.channel.send("Image Saved!")
+  except:
+    db[picName] = link
+    await ctx.channel.send("Image Saved!")
 
 @bot.command()
-async def picG(ctx, picName):
+async def getDB(ctx, picName):
   #getPic
-  if ctx.content.startswith('!picG'):
-    try:
-      await ctx.channel.send(picName)
-      await ctx.channel.send(db[picName])
-    except:
-      await ctx.channel.send("pic does not exist")
+
+  try:
+    await ctx.channel.send(picName)
+    await ctx.channel.send(db[picName])
+  except:
+    await ctx.channel.send("pic does not exist")
+
+@bot.command()
+async def remove(ctx, key):
+
+  del db[key]
+  list()
+  await ctx.channel.send(f"Removed {key}")
+
+@bot.command()
+async def list(ctx):
+  
+  for key in db:
+    values = db[key]
+    await ctx.channel.send(f"{key}: {values}")
 
 
-# class MyClient(discord.Client):
-
-
-#     # trigger each time a message is received
-#     async def on_message(self, message):
-#         print(f'Message from {message.author}: {message.content}')
-
-
-
+  
 bot.run(my_secret)  #to run the bot, param is the bot token/password
+
